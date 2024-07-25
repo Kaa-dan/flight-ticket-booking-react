df8278e3dcf00a55437ac72c089b09819f8592bf
KAADAN <kaadan@localhost.localdomain>
2024-07-25 13:06:49 +0530

    view-booking-ui-completed

--- src/componenets/booking/viewBooking/FlightTickets.jsx
+++ src/componenets/booking/viewBooking/FlightTickets.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import FlightLogo from "../../../assets/booking/viewBookings/flightLogo.png";
+import { MdOutlineDateRange } from "react-icons/md";
+import { BsDoorClosedFill } from "react-icons/bs";
+import { IoIosTime } from "react-icons/io";
+import { MdAirlineSeatReclineExtra } from "react-icons/md";
 const FlightTicket = () => {
   return (
     <div className="flex items-center justify-between p-4 border rounded-lg shadow-md bg-white">
@@ -9,7 +13,7 @@ const FlightTicket = () => {
           className="h-16 w-16 rounded-lg p-1 object-contain mr-4 border border-blue-700"
         />
 
-        <div className="bg-yellow-400 flex gap-2 items-center ">
+        <div className=" flex gap-3 items-center ">
           <div className="flex justify-center items-center gap-1 ">
             <div>
               <div className="text-lg font-semibold">Destination 1</div>
@@ -17,7 +21,7 @@ const FlightTicket = () => {
               <div className="text-lg font-semibold">Destination 2</div>
             </div>
           </div>
-          <div>----</div>
+          <div className="border w-6 border-black"></div>
           <div className="flex items-center space-x-4">
             <div className="flex flex-col items-end">
               <div className="text-xl font-bold">12:00 pm</div>
@@ -27,29 +31,53 @@ const FlightTicket = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-center space-x-6">
-        <div className="text-center">
-          <div className="text-sm text-gray-500">Date</div>
-          <div className="font-medium">12-11-22</div>
+      <div className="flex items-center  space-x-6 ">
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-1">
+            <div className="text-[1.5rem] text-sky-600 bg-slate-300 p-1 rounded-md">
+              <MdOutlineDateRange />
+            </div>
+            <div>
+              <div className="text-sm text-gray-500">Date</div>
+              <div className="font-medium">12-11-22</div>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="text-[1.5rem] text-sky-600 bg-slate-300 p-1 rounded-md">
+              <IoIosTime />
+            </div>
+            <div>
+              <div className="text-sm text-gray-500">Flight time</div>
+              <div className="font-medium">Newark</div>
+            </div>
+          </div>
         </div>
-        <div className="text-center">
-          <div className="text-sm text-gray-500">Gate</div>
-          <div className="font-medium">A12</div>
-        </div>
-        <div className="text-center">
-          <div className="text-sm text-gray-500">Flight time</div>
-          <div className="font-medium">Newark(EWR)</div>
-        </div>
-        <div className="text-center">
-          <div className="text-sm text-gray-500">Seat no.</div>
-          <div className="font-medium">128</div>
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-1 ">
+            <div className="text-[1.5rem] text-sky-600 bg-slate-300 p-1 rounded-md">
+              <BsDoorClosedFill />
+            </div>
+            <div>
+              <div className="text-sm text-gray-500">Gate</div>
+              <div className="font-medium">A12</div>
+            </div>
+          </div>
+          <div className="flex items-center gap-1">
+            <div className="text-[1.5rem] text-sky-600 bg-slate-300 p-1 rounded-md">
+              <MdAirlineSeatReclineExtra />
+            </div>
+            <div>
+              <div className="text-sm text-gray-500">Seat no</div>
+              <div className="font-medium">128</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex items-center space-x-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+        <button className="bg-[#007EC4] text-white px-4 py-2 rounded-sm">
           Download Ticket
         </button>
-        <button className="bg-transparent border border-blue-500 text-blue-500 px-2 py-2 rounded-lg">
+        <button className="bg-transparent border border-[#007EC4] t px-4 py-2 rounded-sm">
           &gt;
         </button>
       </div>
--- src/pages/profile/ViewBooking.jsx
+++ src/pages/profile/ViewBooking.jsx
@@ -41,39 +41,39 @@ const FlightBookings = () => {
   ];
 
   return (
-    <div className=" bg-violet-400">
+    <div className=" ">
       <Header />
       <div className="mx-[7vw] my-5">
-        <div className="flex justify-between items-center rounded-lg bg-red-800 mb-5 px-3 py-4">
-          <div className="flex h-12 space-x-1 justify-between bg-slate-500 w-full">
+        <div className="flex justify-between items-center rounded-lg  mb-5 px-3 py-4 border ">
+          <div className="flex h-12 space-x-1 justify-between bg-slate-500 w-full ">
             <button className="py-2 px-6 w-1/4 bg-blue-600 text-white ">
               Flights
             </button>
-            <button className="py-2 w-1/4 px-6 bg-gray-200 text-gray-600 ">
+            <button className="py-2 w-1/4 px-6 bg-white text-gray-600 ">
               Helicopters
             </button>
-            <button className="py-2 w-1/4 px-6 bg-gray-200 text-gray-600 ">
+            <button className="py-2 w-1/4 px-6 bg-white text-gray-600 ">
               Air Ambulance
             </button>
-            <button className="py-2 w-1/4 px-6 bg-gray-200 text-gray-600 ">
+            <button className="py-2 w-1/4 px-6 bg-white text-gray-600 ">
               Charter Flight
             </button>
           </div>
         </div>
-        <div className="flex justify-between h-10 items-center bg-yellow-500 mb-3 p-2 rounded-lg ">
+        <div className="flex justify-between h-10 items-center mb-3 p-2 rounded-lg border">
           <div className="flex h-full w-full justify-between">
-            <button className="py-2  w-1/3 px-6  text-white border-b-2 border-blue-900">
+            <button className="py-2  w-1/3 px-6  text-gray-600 border-b-2 border-blue-900">
               Upcoming
             </button>
-            <button className="py-2 w-1/3  px-6 bg-gray-200 text-gray-600 border-l-2 border-slate-300 ">
+            <button className="py-2 w-1/3  px-6 bg-white text-gray-600 border-l-2 border-slate-300 ">
               Completed
             </button>
-            <button className="py-2 w-1/3  px-6 bg-gray-200 text-gray-600 border-l-2 border-slate-300">
+            <button className="py-2 w-1/3  px-6 bg-whitetext-gray-600 border-l-2 border-slate-300">
               Cancelled
             </button>
           </div>
         </div>
-        <div className="bg-pink-500 p-4 rounded-lg shadow-md">
+        <div className=" px-4 pt-3 pb-7  rounded-lg shadow-md">
           <h2 className="text-lg font-bold mb-3">
             <a href="">Bookings</a> / <a href="">Flights </a> /{" "}
             <a href="">Upcoming</a>
